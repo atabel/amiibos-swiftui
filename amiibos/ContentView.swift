0359e71efa844a51a18af6735d2baba71a2f6ca7
Abel Toledano <atabel87@gmail.com>
2020-07-20 14:59:17 +0200

    Initial Commit

---
+++ amiibos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  amiibos
+//
+//  Created by Abel Toledano on 20/07/2020.
+//  Copyright © 2020 Abel Toledano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
